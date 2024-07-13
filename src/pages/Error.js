3ff9e313cf32cb568dbfe05faba14864b0428ef1
jkgyso <jkgyso@gmail.com>
2024-07-13 21:16:25 +0800

    Added error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,16 +1,16 @@
-// import { Row, Col } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-// export default function Error() {
+export default function Error() {
 
 
-//     return (
-//         <Row>
-//             <Col className="mt-5 pt-5 text-center mx-auto">
-//                 <h1>404 Error</h1>
-//                 <p>Page Not Found</p>
-//                 <Link className="btn btn-primary" to={"/"}>Return to Home</Link>
-//             </Col>
-//         </Row>
-//     )
-// }
+    return (
+        <Row>
+            <Col className="mt-5 pt-5 text-center mx-auto">
+                <h1>404 Error</h1>
+                <p>Page Not Found</p>
+                <Link className="btn btn-primary" to={"/"}>Return to Home</Link>
+            </Col>
+        </Row>
+    )
+}
